e6fcf3cafb2d31030db6a0fb7c1fa7761ae7cd07
Radim Svoboda <radim.svoboda@emplifi.com>
2021-12-28 16:23:12 +0100

    customize theme's primary color

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,13 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './app';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { blue } from '@mui/material/colors';
 
 const queryClient = new QueryClient();
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: blue[900],
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('app')
