7f572933d4be83585cf9eebac833994aa74b451f
Radim Svoboda <radim.svoboda@emplifi.com>
2021-12-28 13:24:31 +0100

    setup fe with parceljs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
+  env: {
+    node: true,
+  },
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   parserOptions: {
     ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
@@ -7,6 +10,8 @@ module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    'eslint:recommended',
+    'plugin:react/recommended',
   ],
   rules: {
     // 'no-unused-vars': 'off',
